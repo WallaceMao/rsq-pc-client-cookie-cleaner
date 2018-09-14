1c98ad6406635400412c7da371f49b09edd0ec62
Wallace Mao <maowenqiang0752@163.com>
2018-09-14 17:49:28 +0800

    test in mac

--- clean-cookie.sh
+++ clean-cookie.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
-rsq_path="~/Library/Application Support/rishiqing"
+#!/bin/bash
+rsq_path=~/Library/Application\ Support/rishiqing
 rsq_cookie_path=$rsq_path/Cookies
 rsq_cookie_journal_path=$rsq_path/Cookies-journal
 rsq_exe_program=rishiqing
 echo INFO: begin to clean rishiqing cache file
 
 # check rsq_path exist
-if [ ! -d $rsq_path ]
+if [ ! -d "$rsq_path" ]
 then
   echo WARN: rishiqing path not exist
   exit 0
@@ -26,20 +26,20 @@ then
 fi
 
 # delete cookie file
-if [ -e $rsq_cookie_path ]
+if [ -e "$rsq_cookie_path" ]
 then
   echo INFO: deleting file: $rsq_cookie_path
-  rm -rf $rsq_cookie_path
+  rm -rf "$rsq_cookie_path"
   echo INFO: done delete file $rsq_cookie_path
 else
   echo WARN: $rsq_cookie_path not found
 fi
 
 # delete cookie journal file
-if [ -e $rsq_cookie_journal_path ]
+if [ -e "$rsq_cookie_journal_path" ]
 then
   echo INFO: deleting file: $rsq_cookie_journal_path
-  rm -rf $rsq_cookie_journal_path
+  rm -rf "$rsq_cookie_journal_path"
   echo INFO: done delete file $rsq_cookie_journal_path
 else
   echo WARN: $rsq_cookie_journal_path not found
