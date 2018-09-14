6c1b2e61bb4a4e4656e0dc4947e49f34d18746ad
Wallace Mao <maowenqiang0752@163.com>
2018-09-14 16:16:39 +0800

    add cleaner for mac

---
+++ clean-cookie.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+rsq_path=~/AppData/Roaming/rishiqing
+rsq_cookie_path=$rsq_path/Cookies
+rsq_cookie_journal_path=$rsq_path/Cookies-journal
+rsq_exe_program=rishiqing
+echo INFO: begin to clean rishiqing cache file
+
+# check rsq_path exist
+if [ ! -d $rsq_path ]
+then
+  echo WARN: rishiqing path not exist
+  exit 0
+fi
+
+echo cleaning......
+# kill rishiqing.exe process
+rsq_program_id=`ps -ef | grep ${rsq_exe_program} | grep -v grep | awk '{print $2}'`
+if [ "$rsq_program_id" != "" ]
+then
+  echo INFO: killing process pid $rsq_program_id
+  kill -9 $rsq_program_id
+  sleep 5
+fi
+
+# delete cookie file
+if [ -e $rsq_cookie_path ]
+then
+  echo INFO: deleting file: $rsq_cookie_path
+  rm -rf $rsq_cookie_path
+  echo INFO: done delete file $rsq_cookie_path
+else
+  echo WARN: $rsq_cookie_path not found
+fi
+
+# delete cookie journal file
+if [ -e $rsq_cookie_journal_path ]
+then
+  echo INFO: deleting file: $rsq_cookie_journal_path
+  rm -rf $rsq_cookie_journal_path
+  echo INFO: done delete file $rsq_cookie_journal_path
+else
+  echo WARN: $rsq_cookie_journal_path not found
+fi
+
+exit 0
